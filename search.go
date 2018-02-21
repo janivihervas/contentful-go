c964117b1c67eedb4bdfdff0ec970158f649929c
Jani Viherväs <jani.vihervas@futurice.com>
2018-02-21 18:41:41 +0200

    CLI program for testing

--- search.go
+++ search.go
@@ -2,9 +2,16 @@ package contentful
 
 import (
 	"context"
+	"fmt"
 	"net/url"
 )
 
 func (c *client) Search(ctx context.Context, parameters url.Values, data interface{}) error {
+	if parameters == nil {
+		parameters = url.Values{}
+	}
+	parameters.Add("include", "10")
+
+	fmt.Println("Params:", parameters.Encode())
 	return nil
 }
