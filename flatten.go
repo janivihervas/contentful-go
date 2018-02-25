8cf4579c930799754f1a77dc5e6c23ca4a467297
Jani Viherväs <janivihervas@users.noreply.github.com>
2018-02-25 15:32:36 +0200

    Extra fields (#6)
    * Remove log
    
    * Inject extra fields to the result

--- flatten.go
+++ flatten.go
@@ -72,6 +72,14 @@ func flattenItem(includes includes, item item) (map[string]interface{}, error) {
 		}
 		flattenedFields[key] = flattenedField
 	}
+
+	flattenedFields["contentful_id"] = item.Sys.ID
+	flattenedFields["contentful_contentType"] = item.Sys.ContentType.Sys.ID
+	flattenedFields["contentful_revision"] = item.Sys.Revision
+	flattenedFields["contentful_createdAt"] = item.Sys.CreatedAt
+	flattenedFields["contentful_updatedAt"] = item.Sys.UpdatedAt
+	flattenedFields["contentful_locale"] = item.Sys.Locale
+
 	return flattenedFields, nil
 }
 
