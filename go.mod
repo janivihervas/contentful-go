192bf9d62b1818933424ce19ba86379a3a47213f
Jani Viherväs <janivihervas@users.noreply.github.com>
2018-10-16 20:52:41 +0300

    Add retry logic (#14)
    * Add retry logic
    
    * Rename var
    
    * AUTOMATIC BOT: Install
    
    * AUTOMATIC BOT: Format

--- go.mod
+++ go.mod
@@ -4,10 +4,10 @@ require (
 	github.com/alexkohler/nakedret v0.0.0-20171106223215-c0e305a4f690 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/fzipp/gocyclo v0.0.0-20150627053110-6acd4345c835 // indirect
-	github.com/golang/lint v0.0.0-20180702182130-06c8688daad7 // indirect
+	github.com/golang/lint v0.0.0-20181011164241-5906bd5c48cd // indirect
 	github.com/kisielk/errcheck v1.1.0 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/stretchr/testify v1.2.2
-	golang.org/x/lint v0.0.0-20180702182130-06c8688daad7 // indirect
-	golang.org/x/tools v0.0.0-20181010000725-29f11e2b93f4 // indirect
+	golang.org/x/lint v0.0.0-20181011164241-5906bd5c48cd // indirect
+	golang.org/x/tools v0.0.0-20181016152912-4fd33079060a // indirect
 )
--- search_test.go
+++ search_test.go
@@ -2,14 +2,12 @@ package contentful
 
 import (
 	"context"
-	"testing"
-
+	"encoding/json"
+	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
-
-	"io/ioutil"
-
-	"encoding/json"
+	"testing"
+	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -26,10 +24,10 @@ func TestContentful_GetServerFails(t *testing.T) {
 	)
 
 	err := cms.GetMany(ctx, Parameters(), &result)
-	assert.NotNil(t, err)
+	assert.Error(t, err)
 
 	err = cms.GetOne(ctx, Parameters(), &result)
-	assert.NotNil(t, err)
+	assert.Error(t, err)
 }
 
 func TestContentful_GetWrongTotal(t *testing.T) {
@@ -43,7 +41,7 @@ func TestContentful_GetWrongTotal(t *testing.T) {
 		server   = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusOK)
 			err := json.NewEncoder(w).Encode(response)
-			assert.Nil(t, err)
+			assert.NoError(t, err)
 		}))
 		cms = Contentful{
 			token:   "token",
@@ -113,10 +111,10 @@ func TestContentful_GetParseFails(t *testing.T) {
 	t.Parallel()
 
 	bytes, err := ioutil.ReadFile("testdata/reference_fields.json")
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 	fields := make(map[string]interface{})
 	err = json.Unmarshal(bytes, &fields)
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 
 	var (
 		response = searchResults{
@@ -130,7 +128,7 @@ func TestContentful_GetParseFails(t *testing.T) {
 		server = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusOK)
 			err := json.NewEncoder(w).Encode(response)
-			assert.Nil(t, err)
+			assert.NoError(t, err)
 		}))
 		cms = Contentful{
 			token:   "token",
@@ -144,9 +142,9 @@ func TestContentful_GetParseFails(t *testing.T) {
 	defer server.Close()
 
 	err = cms.GetMany(ctx, Parameters(), &resultMany)
-	assert.NotNil(t, err)
+	assert.Error(t, err)
 	err = cms.GetOne(ctx, Parameters(), &resultOne)
-	assert.NotNil(t, err)
+	assert.Error(t, err)
 }
 
 func TestContentful_GetUnMarshalFails(t *testing.T) {
@@ -157,9 +155,9 @@ func TestContentful_GetUnMarshalFails(t *testing.T) {
 		server   = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusOK)
 			bytes, err := ioutil.ReadFile("testdata/" + dataFile)
-			assert.Nil(t, err)
+			assert.NoError(t, err)
 			_, err = w.Write(bytes)
-			assert.Nil(t, err)
+			assert.NoError(t, err)
 		}))
 		cms = Contentful{
 			token:   "token",
@@ -174,12 +172,12 @@ func TestContentful_GetUnMarshalFails(t *testing.T) {
 
 	dataFile = "preview_all_pages.json"
 	err := cms.GetMany(ctx, Parameters(), &resultOne)
-	assert.NotNil(t, err)
+	assert.Error(t, err)
 	assert.Contains(t, err.Error(), "json: cannot unmarshal")
 
 	dataFile = "preview_main_page.json"
 	err = cms.GetOne(ctx, Parameters(), &resultMany)
-	assert.NotNil(t, err)
+	assert.Error(t, err)
 	assert.Contains(t, err.Error(), "json: cannot unmarshal")
 }
 
@@ -191,9 +189,9 @@ func TestContentful_Get(t *testing.T) {
 		server   = httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusOK)
 			bytes, err := ioutil.ReadFile("testdata/" + dataFile)
-			assert.Nil(t, err)
+			assert.NoError(t, err)
 			_, err = w.Write(bytes)
-			assert.Nil(t, err)
+			assert.NoError(t, err)
 		}))
 		cms = Contentful{
 			token:   "token",
@@ -207,7 +205,7 @@ func TestContentful_Get(t *testing.T) {
 	resultMany := make([]map[string]interface{}, 1)
 	dataFile = "preview_all_pages.json"
 	err := cms.GetMany(ctx, Parameters(), &resultMany)
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 	assert.Equal(t, 3, len(resultMany))
 
 	cases := []struct {
@@ -236,11 +234,11 @@ func TestContentful_Get(t *testing.T) {
 	resultOne := make(map[string]interface{})
 	dataFile = "preview_all_pages.json"
 	err = cms.GetOne(ctx, Parameters(), &resultOne)
-	assert.NotNil(t, err)
+	assert.Error(t, err)
 
 	dataFile = "preview_main_page.json"
 	err = cms.GetOne(ctx, Parameters(), &resultOne)
-	assert.Nil(t, err)
+	assert.NoError(t, err)
 
 	cases = []struct {
 		expected interface{}
@@ -271,15 +269,15 @@ func TestContentful_search(t *testing.T) {
 		ctx = context.Background()
 	)
 
-	t.Run("Should return an error if no url is set", func(tt *testing.T) {
+	t.Run("Should return an error if no url is set", func(t *testing.T) {
 		_, err := cms.search(ctx, SearchParameters{})
-		assert.NotNil(tt, err)
+		assert.Error(t, err)
 	})
 
-	t.Run("Client should call the correct endpoint with correct bearer token", func(tt *testing.T) {
+	t.Run("Client should call the correct endpoint with correct bearer token", func(t *testing.T) {
 		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-			assert.Equal(tt, "/spaces/spaceID/entries?include=10", r.URL.Path+"?"+r.URL.RawQuery)
-			assert.Equal(tt, "Bearer token", r.Header.Get("Authorization"))
+			assert.Equal(t, "/spaces/spaceID/entries?include=10", r.URL.Path+"?"+r.URL.RawQuery)
+			assert.Equal(t, "Bearer token", r.Header.Get("Authorization"))
 			w.WriteHeader(http.StatusInternalServerError)
 		}))
 		defer server.Close()
@@ -288,7 +286,7 @@ func TestContentful_search(t *testing.T) {
 		_, _ = cms.search(ctx, Parameters())
 	})
 
-	t.Run("Should return an error if Contentful responds with non-200 status code", func(tt *testing.T) {
+	t.Run("Should return an error if Contentful responds with non-200 status code", func(t *testing.T) {
 		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusInternalServerError)
 		}))
@@ -296,11 +294,11 @@ func TestContentful_search(t *testing.T) {
 
 		cms.url = server.URL
 		_, err := cms.search(ctx, Parameters())
-		assert.NotNil(tt, err)
-		assert.Contains(tt, err.Error(), "500")
+		assert.Error(t, err)
+		assert.Contains(t, err.Error(), "500")
 	})
 
-	t.Run("Should return an error if body can't be read", func(tt *testing.T) {
+	t.Run("Should return an error if body can't be read", func(t *testing.T) {
 		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusOK)
 			w.Write([]byte{})
@@ -309,26 +307,164 @@ func TestContentful_search(t *testing.T) {
 
 		cms.url = server.URL
 		_, err := cms.search(ctx, Parameters())
-		assert.NotNil(tt, err)
+		assert.Error(t, err)
 	})
 
-	t.Run("Should not return an error and should return correctly parsed search results", func(tt *testing.T) {
+	t.Run("Should not return an error and should return correctly parsed search results", func(t *testing.T) {
 		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 			w.WriteHeader(http.StatusOK)
 			bytes, err := ioutil.ReadFile("testdata/prod_all_pages.json")
-			assert.Nil(tt, err)
+			assert.NoError(t, err)
 			_, err = w.Write(bytes)
-			assert.Nil(tt, err)
+			assert.NoError(t, err)
 		}))
 		defer server.Close()
 
 		cms.url = server.URL
 		response, err := cms.search(ctx, Parameters())
-		assert.Nil(tt, err)
-		assert.Equal(tt, 2, response.Total)
-		assert.Equal(tt, 0, response.Skip)
-		assert.Equal(tt, 100, response.Limit)
-		assert.Equal(tt, 2, len(response.Items))
+		assert.NoError(t, err)
+		assert.Equal(t, 2, response.Total)
+		assert.Equal(t, 0, response.Skip)
+		assert.Equal(t, 100, response.Limit)
+		assert.Equal(t, 2, len(response.Items))
+	})
+
+	t.Run("Should return ErrTooManyRequests if retry shouldn't be tried", func(t *testing.T) {
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			w.WriteHeader(http.StatusTooManyRequests)
+		}))
+		defer server.Close()
+
+		cms.url = server.URL
+
+		t.Run("No deadline set on context", func(t *testing.T) {
+			_, err := cms.search(context.Background(), Parameters())
+			assert.Error(t, err)
+			assert.Equal(t, ErrTooManyRequests, err)
+		})
+
+		t.Run("Deadline is before the retry seconds", func(t *testing.T) {
+			c, cancel := context.WithTimeout(context.Background(), time.Second)
+			defer cancel()
+			_, err := cms.search(c, Parameters())
+			assert.Error(t, err)
+			assert.Equal(t, ErrTooManyRequests, err)
+		})
+	})
+
+	t.Run("Should retry if hits Contentful rate limit", func(t *testing.T) {
+		var (
+			rateLimit       = true
+			happyCaseCalled = false
+		)
+
+		server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if rateLimit {
+				w.Header().Set("X-Contentful-RateLimit-Reset", "1")
+				w.WriteHeader(http.StatusTooManyRequests)
+				rateLimit = false
+				return
+			}
+
+			happyCaseCalled = true
+
+			w.WriteHeader(http.StatusOK)
+			bytes, err := ioutil.ReadFile("testdata/prod_all_pages.json")
+			assert.NoError(t, err)
+			_, err = w.Write(bytes)
+			assert.NoError(t, err)
+		}))
+		defer server.Close()
+
+		cms.url = server.URL
+
+		c, cancel := context.WithTimeout(context.Background(), time.Second*5)
+		defer cancel()
+		_, err := cms.search(c, Parameters())
+		assert.NoError(t, err)
+		assert.True(t, happyCaseCalled)
+	})
+}
+
+func TestContentful_retryAfter(t *testing.T) {
+	t.Parallel()
+
+	t.Run("If deadline is not set on context, will always return -1", func(t *testing.T) {
+		ctx := context.TODO()
+		assert.Equal(t, -1, retryAfter(ctx, nil))
+
+		ctx = context.Background()
+		assert.Equal(t, -1, retryAfter(ctx, nil))
+
+		ctx = context.WithValue(context.Background(), struct{}{}, "bar")
+		assert.Equal(t, -1, retryAfter(ctx, nil))
+	})
+
+	t.Run("If deadline is set on context, will not return -1", func(t *testing.T) {
+		ctx1, cancel1 := context.WithDeadline(context.Background(), time.Now().Add(time.Hour*1))
+		defer cancel1()
+		assert.NotEqual(t, -1, retryAfter(ctx1, nil))
+
+		ctx2, cancel2 := context.WithTimeout(context.Background(), time.Hour*1)
+		defer cancel2()
+		assert.NotEqual(t, -1, retryAfter(ctx2, nil))
+	})
+
+	t.Run("If header can't be parsed, returns 2", func(t *testing.T) {
+		ctx, cancel := context.WithTimeout(context.Background(), time.Hour*1)
+		defer cancel()
+		assert.Equal(t, 2, retryAfter(ctx, nil))
+
+		resp := &http.Response{
+			Header: make(http.Header),
+		}
+		assert.Equal(t, 2, retryAfter(ctx, resp))
+
+		resp.Header.Set("X-Contentful-RateLimit-Reset", "foo")
+		assert.Equal(t, 2, retryAfter(ctx, resp))
+
+		resp.Header.Set("X-Contentful-RateLimit-Reset", "5")
+		assert.NotEqual(t, 2, retryAfter(ctx, resp))
+	})
+
+	t.Run("If deadline of context would end before retry, will return -1", func(t *testing.T) {
+		t.Run("Header not ok", func(t *testing.T) {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*1)
+			defer cancel()
+
+			assert.Equal(t, -1, retryAfter(ctx, nil))
+		})
+
+		t.Run("Header ok", func(t *testing.T) {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*5)
+			defer cancel()
+
+			resp := &http.Response{
+				Header: make(http.Header),
+			}
+			resp.Header.Set("X-Contentful-RateLimit-Reset", "10")
+			assert.Equal(t, -1, retryAfter(ctx, resp))
+		})
+	})
+
+	t.Run("If deadline of context will end after retry, will return retry seconds", func(t *testing.T) {
+		t.Run("Header not ok", func(t *testing.T) {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+			defer cancel()
+
+			assert.Equal(t, 2, retryAfter(ctx, nil))
+		})
+
+		t.Run("Header not ok", func(t *testing.T) {
+			ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+			defer cancel()
+
+			resp := &http.Response{
+				Header: make(http.Header),
+			}
+			resp.Header.Set("X-Contentful-RateLimit-Reset", "5")
+			assert.Equal(t, 5, retryAfter(ctx, resp))
+		})
 	})
 }
 
